68e3f6b50c9db4d0d5ab4a8167dca427fbdddf06
David <david.olanders@live.se>
2019-11-25 14:16:39 +0100

    Part 2

--- code/gpreg.py
+++ code/gpreg.py
@@ -7,20 +7,23 @@ Gaussian process classification (GPC)
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pdb
 from sklearn import datasets
-from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.gaussian_process.kernels import DotProduct
+from keras.utils import to_categorical
 # import some data to play with
 digits = datasets.load_digits()
 
 X = digits.data
 y = np.array(digits.target, dtype = int)
+y = to_categorical(y)
 
 N,d = X.shape
 
-N = np.int(1000)
-Ntrain = np.int(900)
+N = np.int(600)
+Ntrain = np.int(500)
 Ntest = np.int(100)
 
 
@@ -30,13 +33,16 @@ Xtest = X[Ntrain:N,:]
 ytest = y[Ntrain:N]
 
 
-kernel = 1.0 * RBF([3]) # isotropic kernel, 1-3.5
+kernel = 1.0 * RBF([1]) # isotropic kernel, 1-3.5
 #kernel = DotProduct(1)
-gpc_rbf = GaussianProcessClassifier(kernel=kernel).fit(Xtrain, ytrain)
+gpc_rbf = GaussianProcessRegressor(kernel=kernel).fit(Xtrain, ytrain)
 yp_train = gpc_rbf.predict(Xtrain)
-train_error_rate = np.mean(np.not_equal(yp_train,ytrain))
+yp_train_one_hot = (yp_train == yp_train.max(axis=1)[:,None]).astype(int)
+#print(sum(yp_train))
+train_error_rate = np.mean(np.not_equal(yp_train_one_hot,ytrain))
 yp_test = gpc_rbf.predict(Xtest)
-test_error_rate = np.mean(np.not_equal(yp_test,ytest))
+yp_test_one_hot = (yp_test == yp_test.max(axis=1)[:,None]).astype(int)
+test_error_rate = np.mean(np.not_equal(yp_test_one_hot,ytest))
 print('Training error rate')
 print(train_error_rate)
 print('Test error rate')
